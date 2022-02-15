d1cf3dc5df14f07d48b4f4973df8f30c110957bd
Pablo Klaschka <contact@pabloklaschka.de>
2022-02-15 20:05:54 +0100

    feat(network-devices-overview-extension): Add basic implementation

--- packages/network-devices-overview-extension/src/index.ts
+++ packages/network-devices-overview-extension/src/index.ts
@@ -1,20 +1,23 @@
+import { networkInterfaces } from 'os';
 import { Extension } from '@server-state/types';
-// import typeDefs from './schema.graphql';
+import typeDefs from './schema.graphql';
 
 const extension: Extension<Record<string, never>> = async () => {
-	// const resolvers = {
-	// 	ServerState: {
-	// 		NETDEVSO: () => ({})
-	// 	},
-	// };
+	const resolvers = {
+		ServerState: {
+			NETDEVSO: () => ({})
+		},
+		NETDEVSO_TYPE: () => {
+			return {
+				interfaces: JSON.stringify(networkInterfaces())
+			};
+		}
+	};
 
 	return {
-		name: 'Network Devices Overview'
-		// onRemove: async () => {
-		//
-		// }
-		// typeDefs,
-		// resolvers
+		name: 'Network Devices Overview (NETDEVSO)',
+		typeDefs,
+		resolvers
 	};
 };
 
